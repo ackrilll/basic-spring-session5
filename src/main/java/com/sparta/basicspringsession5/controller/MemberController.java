10af58b1fb1324b1526d77d6df23d834019ca0f6
Ackrilll <skawlsgus2@naver.com>
2024-09-10 13:18:41 +0900

    [UPDATE] 기본 구조 세팅

---
+++ src/main/java/com/sparta/basicspringsession5/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.sparta.basicspringsession5.controller;
+
+import com.sparta.basicspringsession5.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+}
---
+++ src/main/java/com/sparta/basicspringsession5/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.sparta.basicspringsession5.service;
+
+import com.sparta.basicspringsession5.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
