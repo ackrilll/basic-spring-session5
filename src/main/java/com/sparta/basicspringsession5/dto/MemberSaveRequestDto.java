4b8d705440c5324cef97971f9b9179e6d44e8b9f
Ackrilll <skawlsgus2@naver.com>
2024-09-10 13:42:49 +0900

    [UPDATE] CRUD 기능 추가

---
+++ src/main/java/com/sparta/basicspringsession5/dto/MemberSaveRequestDto.java
@@ -0,0 +1,8 @@
+package com.sparta.basicspringsession5.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberSaveRequestDto {
+    private String name;
+}
---
+++ src/main/java/com/sparta/basicspringsession5/dto/MemberUpdateRequestDto.java
@@ -0,0 +1,8 @@
+package com.sparta.basicspringsession5.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberUpdateRequestDto {
+    private String name;
+}
