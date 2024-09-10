ca8c04dcc876a96e0098cadace131764f2a3f166
Ackrilll <skawlsgus2@naver.com>
2024-09-10 13:14:46 +0900

    êinit

---
+++ src/main/java/com/sparta/basicspringsession5/BasicSpringSession5Application.java
@@ -0,0 +1,13 @@
+package com.sparta.basicspringsession5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSession5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringSession5Application.class, args);
+    }
+
+}
